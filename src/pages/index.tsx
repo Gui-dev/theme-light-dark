cb1317d34dec98d260d46c359a07bb0a699aa530
Gui-dev <adrianon10@gmail.com>
2020-12-08 03:49:42 -0300

    feat: add Container style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  color: red;
-  font-size: 50px;
-`
+import { Container } from './style'
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <Container>
+      <h1>Minha página</h1>
+    </Container>
+  )
 }
